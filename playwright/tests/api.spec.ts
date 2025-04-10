8f651bcbefd21526ae238d0cd6c2208490978750
Lilyana Valcheva <lilyanavalcheva@Lilyanas-MacBook-Air.local>
2025-04-10 22:13:22 +0300

    API Testing part 2

--- playwright/tests/api.spec.ts
+++ playwright/tests/api.spec.ts
@@ -1,25 +1,52 @@
 import { test, expect } from "@playwright/test";
+import { REQUEST_URL, PRODUCT_ID } from "../test-data/constants.ts";
+import REQUEST_BODY from "../test-data/request_body.json";
 
-test("API Test - GET Products", async ({ request }) => {
-  const requestURL = "https://fakestoreapi.com/products";
-  const response = await request.get(requestURL);
-  const responseBody = await response.json();
-  await expect(response.ok()).toBeTruthy();
-  await expect(response.status()).toBe(200);
-  await expect(responseBody).toContainEqual(
-    expect.objectContaining({
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    })
-  );
+test.describe("Matchers example", async () => {
+  test("toContainEqual example", async () => {
+    const users = [
+      { id: 1, name: "Alice" },
+      { id: 2, name: "Bob" },
+    ];
+    expect(users).toContainEqual({ id: 1, name: "Alice" });
+  });
+
+  test("toEqual example", async () => {
+    expect({ name: "Alice", age: 30 }).toEqual({
+      name: "Alice",
+      age: 30,
+    });
+  });
+
+  test("toMatchObject example", async () => {
+    const user = {
+      id: 1,
+      name: "Alice",
+      age: 30,
+    };
+
+    expect(user).toMatchObject({ name: "Alice" });
+  });
+
+  test("toHaveProperty example", async () => {
+    const user = {
+      profile: {
+        name: "Alice",
+      },
+    };
+
+    expect(user).toHaveProperty("profile.name");
+    expect(user).toHaveProperty("profile.name", "Alice");
+  });
 });
 
-test.only("API Test - GET Product #5", async ({ request }) => {
+test("API Test - GET Product #5", async ({ request }) => {
   const requestURL5 = "https://fakestoreapi.com/products/5";
   const response5 = await request.get(requestURL5);
   const responseBody5 = await response5.json();
-  await expect(response5.ok()).toBeTruthy();
-  await expect(response5.status()).toBe(200);
-  await expect(responseBody5).toMatchObject(
+  expect(response5.ok()).toBeTruthy();
+  expect(response5.status()).toBe(200);
+  expect(responseBody5).toMatchObject(
     expect.objectContaining({
       title:
         "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
@@ -27,4 +54,92 @@ test.only("API Test - GET Product #5", async ({ request }) => {
       price: 695,
     })
   );
+});
+
+test.describe("CRUD Operations", () => {
+  test("API Test - GET Products", async ({ request }) => {
+    const requestURL = REQUEST_URL;
+    const response = await request.get(requestURL);
+    const responseBody = await response.json();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    expect(responseBody).toContainEqual(
+      expect.objectContaining({
+        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      })
+    );
+  });
+  test("API TEST 2 - GET Product", async ({ request }) => {
+    const response = await request.get(`${REQUEST_URL}/${PRODUCT_ID}`);
+    const responseBody = await response.json();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        title: "White Gold Plated Princess",
+        id: 7,
+        price: 9.99,
+      })
+    );
+
+    expect(responseBody).toMatchObject({
+      category: "jewelery",
+    });
+
+    expect(responseBody).toHaveProperty("image");
+    expect(responseBody).toHaveProperty("rating.rate");
+  });
+
+  test("API TEST 3 = CREATE Product", async ({ request }) => {
+    const response = await request.post(REQUEST_URL, {
+      data: REQUEST_BODY,
+    });
+
+    const responseBody = await response.json();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    expect(responseBody).toEqual({
+      id: 21,
+      title: "string",
+      price: 0.1,
+      description: "string",
+      image: "http://example.com",
+      category: "string",
+    });
+
+    expect(responseBody).toHaveProperty("price", 0.1);
+  });
+
+  test("API TEST 4 - UPDATE PRODUCT", async ({ request }) => {
+    const response = await request.put(`${REQUEST_URL}/${PRODUCT_ID}`, {
+      data: {
+        id: 6,
+        title: "Test",
+        price: 0.3,
+        description: "Test",
+        category: "string",
+        image: "http://example.com",
+      },
+    });
+
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(response.ok()).toBeTruthy();
+
+    expect(response.status()).toBe(200);
+
+    expect(responseBody).toMatchObject({
+      title: "Test",
+      price: 0.3,
+      description: "Test",
+    });
+  });
+
+  test("API TEST 5 - DELETE Product", async ({ request }) => {
+    const response = await request.delete(`${REQUEST_URL}/${PRODUCT_ID}`);
+    const responseBody = await response.json();
+    console.log(responseBody);
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+  });
 });
